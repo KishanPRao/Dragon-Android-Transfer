94f999bf363ef764b50df169da43fbd7808e70be
Kishan P Rao <kishan701a@gmail.com>
2018-07-18 18:12:10 +0530

    Updated transfer progress color

--- Dragon Android Transfer/Dragon Android Transfer/UI/Utilities/LightTheme.swift
+++ Dragon Android Transfer/Dragon Android Transfer/UI/Utilities/LightTheme.swift
@@ -37,13 +37,13 @@ class LightTheme: Theme {
     let listSelectedBackgroundColor = color("bfcbff")
     
 //    let transferBg = color("f8f8f8")
-    let transferBg = color("e7f3ff")
+    let transferBg = color("edecec")
     let transferTextColor = color("232323")
-    let transferProgressFg = color("679af7")
+    let transferProgressFg = color("6789f7")
     let transferProgressBg = color("646669")
     
     let dialogBgColor = color("edecec")
-    let dialogWindowColor = color("fafafa")
+    let dialogWindowColor = color("edecec")
     let dialogTextColor = color("2c2c2c")
     let dialogSelectionColor = color("79bdfc")
     let dialogSelectionDangerColor = color("f14545")
